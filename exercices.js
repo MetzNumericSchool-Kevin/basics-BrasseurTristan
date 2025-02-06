6ebae74c98a7b4932daed280d08db6d274cafe1b
BrasseurTristan <brasseur.tristan@gmail.com>
2025-02-06 16:44:26 +0100

    Affichage conditionnel

--- exercices.js
+++ exercices.js
@@ -5,8 +5,12 @@ const store_name = 'À la poche pleine !';
 let potions = 99; // Je suis prévoyant !
 let potion_price = 50;
 let store_status = true;
+const money = '🪙';
 
-
-
+if (store_status) {
+    console.log(`Bienvenue dans la boutique ${store_name} Aventurier ! 🎉`);
+} else {
+    console.log(`La boutique ${store_name} est fermée, revenez plus tard Aventurier ! 😴`);
+}
 
 
