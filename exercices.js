07fe115e20c7f8a221e430e3fd0ab2516f227689
BrasseurTristan <brasseur.tristan@gmail.com>
2025-02-06 16:43:17 +0100

    définition de variables

---
+++ exercices.js
@@ -0,0 +1,12 @@
+const nom_sorcier = prompt("Comment te nommes-tu, sorcier ? 🧙‍♂️");
+console.log("Le sorcier se nomme :" + nom_sorcier + " ! 🎉");
+
+const store_name = 'À la poche pleine !';
+let potions = 99; // Je suis prévoyant !
+let potion_price = 50;
+let store_status = true;
+
+
+
+
+
